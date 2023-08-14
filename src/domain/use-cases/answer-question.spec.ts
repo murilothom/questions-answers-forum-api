7d259476efba737ac8155c7f21914107fbe7e5a7
Murilo Thom <murilothom.2002@hotmail.com>
2023-08-14 13:05:28 -0300

    Use case

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer): Promise<void> => {
+    return
+  },
+}
+
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta'
+  })
+
+  expect(answer.content).toEqual('Nova resposta')
+})
