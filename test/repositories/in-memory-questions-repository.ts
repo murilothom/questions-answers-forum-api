a6053f870263b6f60c70d0c5b811327ecec0927d
Murilo Thom <murilothom.2002@hotmail.com>
2023-08-21 11:06:30 -0300

    get question by slug use case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,10 +1,20 @@
 import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
-import { Question } from '../../src/domain/forum/enterprise/entities/question'
+import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
   async create(question: Question): Promise<void> {
     this.items.push(question)
+  }
+
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
   }
 }
