06e747eff29bc8acdc2aa775e0131c89427cdf37
Murilo Thom <murilothom.2002@hotmail.com>
2023-08-28 11:04:13 -0300

    edit answer use case

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,3 +1,4 @@
+import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/question-repository'
 
 interface EditQuestionUseCaseRequest {
@@ -5,6 +6,10 @@ interface EditQuestionUseCaseRequest {
   questionId: string
   title: string
   content: string
+}
+
+interface EditQuestionUseCaseResponse {
+  question: Question
 }
 
 export class EditQuestionUseCase {
@@ -15,7 +20,7 @@ export class EditQuestionUseCase {
     questionId,
     title,
     content,
-  }: EditQuestionUseCaseRequest): Promise<void> {
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
     const question = await this.questionsRepository.findById(questionId)
 
     if (!question) {
@@ -30,5 +35,9 @@ export class EditQuestionUseCase {
     question.content = content
 
     await this.questionsRepository.save(question)
+
+    return {
+      question,
+    }
   }
 }
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -7,4 +7,30 @@ export class InMemoryAnswersRepository implements AnswersRepository {
   async create(answer: Answer): Promise<void> {
     this.items.push(answer)
   }
+
+  async save(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items[itemIndex] = answer
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    if (itemIndex < 0) {
+      return
+    }
+
+    this.items.splice(itemIndex, 1)
+  }
+
+  async findById(answerId: string): Promise<Answer | null> {
+    const item = this.items.find((item) => item.id.toValue() === answerId)
+
+    if (!item) {
+      return null
+    }
+
+    return item
+  }
 }
