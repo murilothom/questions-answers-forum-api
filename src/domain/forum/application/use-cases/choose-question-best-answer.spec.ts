ad328ea351cae85f0baddac920a354f804c9f25e
Murilo Thom <murilothom.2002@hotmail.com>
2023-08-29 09:59:11 -0300

    choose question best answer use case

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -0,0 +1,58 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
+import { makeQuestion } from 'test/factories/make-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: ChooseQuestionBestAnswerUseCase
+
+describe('Choose Question Best Answer', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new ChooseQuestionBestAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryQuestionsRepository,
+    )
+  })
+
+  it('should be able to choose question best answer', async () => {
+    const question = makeQuestion()
+    await inMemoryQuestionsRepository.create(question)
+
+    const answer = makeAnswer({
+      questionId: question.id,
+    })
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      authorId: question.authorId.toString(),
+      answerId: answer.id.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items[0].bestAnswerId).toEqual(answer.id)
+  })
+
+  it('should not be able to choose another user question best answer', async () => {
+    const question = makeQuestion({
+      authorId: new UniqueEntityID('author-1'),
+    })
+    await inMemoryQuestionsRepository.create(question)
+
+    const answer = makeAnswer({
+      questionId: question.id,
+    })
+    await inMemoryAnswersRepository.create(answer)
+
+    expect(
+      async () =>
+        await sut.execute({
+          authorId: 'author-2',
+          answerId: answer.id.toString(),
+        }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
