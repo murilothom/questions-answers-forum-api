2140884738ba1fa4c90682b70ca5c976e9ed6d7b
Murilo Thom <murilothom.2002@hotmail.com>
2023-08-29 09:59:23 -0300

    delete answer use case

---
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -0,0 +1,27 @@
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface DeleteAnswerUseCaseRequest {
+  answerId: string
+  authorId: string
+}
+
+export class DeleteAnswerUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
+
+  async execute({
+    answerId,
+    authorId,
+  }: DeleteAnswerUseCaseRequest): Promise<void> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    await this.answersRepository.delete(answer)
+  }
+}
