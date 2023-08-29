7050d00cc72c4f350bd8159288b4b62962a0c1de
Murilo Thom <murilothom.2002@hotmail.com>
2023-08-29 12:04:34 -0300

    delete question comment use case

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -2,4 +2,6 @@ import { QuestionComment } from '@/domain/forum/enterprise/entities/question-com
 
 export interface QuestionCommentsRepository {
   create(questionComment: QuestionComment): Promise<void>
+  delete(questionComment: QuestionComment): Promise<void>
+  findById(id: string): Promise<QuestionComment | null>
 }
