1ececd898d95e43c7ea297fad1ce953c36fb35c8
Murilo Thom <murilothom.2002@hotmail.com>
2024-06-20 11:10:47 -0300

    domain events

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,3 +1,20 @@
+import { DomainEvent } from '../events/domain-event'
+import { DomainEvents } from '../events/domaint-events'
 import { Entity } from './entity'
 
-export abstract class AggregateRoot<T> extends Entity<T> {}
+export abstract class AggregateRoot<T> extends Entity<T> {
+  private _domainEvents: DomainEvent[] = []
+
+  get domainEvents(): DomainEvent[] {
+    return this._domainEvents
+  }
+
+  protected addDomainEvent(domainEvent: DomainEvent): void {
+    this._domainEvents.push(domainEvent)
+    DomainEvents.markAggregateForDispatch(this)
+  }
+
+  public clearEvents() {
+    this._domainEvents = []
+  }
+}
